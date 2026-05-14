8498fd897616770dde3e4560b865905504d04f68
jeptoo40 <161425979+jeptoo40@users.noreply.github.com>
2026-05-14 18:06:27 +0300

    Update Signin.jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -28,7 +28,7 @@ export default function SignIn() {
     };
   
     try {
-      const response = await fetch("http://localhost:8000/login.php", {
+      const response = await fetch("https://elimumall.ct.ws/login.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
