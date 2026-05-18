042ebdf444475b07fdf59a44332af656eb78d3c9
jeptoo40 <161425979+jeptoo40@users.noreply.github.com>
2026-05-18 16:36:36 +0300

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -31,7 +31,7 @@ export default function SignUp() {
     };
 
     try {
-     const response = await fetch("https://elimumall.ct.ws/signup.php", {
+     const response = await fetch("https://elimumall.loveslife.biz/signup.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
